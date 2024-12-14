4afd0e46dcf1f2409e8480b2f9226be6404c71f9
Михаил Манузин <klinf157@gmail.com>
2024-12-14 22:34:03 +0300

    added docstring for api.py

--- api.py
+++ api.py
@@ -7,6 +7,21 @@ import logs
 
 
 class ApiGetAndParse:
+    '''
+    Класс для получения и работы с данными о курсах валют, предоставленными
+    API ЦБ РФ.
+    Реализует метод GetCursOnDateXML.
+
+    Переменные:
+        date:str - строка с датой, за которую необходимо запросить курсы
+        в формате ДД.ММ.ГГГГ.
+
+    Методы:
+        get_rates_data,
+        parse_rates_data,
+        get_required_currencies.
+    '''
+
     def __init__(self, date: str) -> None:
         self.date = str(
             datetime.datetime.strptime(date, "%d.%m.%Y")
@@ -28,6 +43,18 @@ class ApiGetAndParse:
     </soap12:Envelope>'''
 
     def get_rates_data(self) -> requests.Response:
+        '''
+        Получает от API данные о курсах валют за заданную дату.
+
+        Возвращает:
+            requests.Response: объект Response
+            с данными API, если код ответа сервера == 200.
+
+        Исключения:
+            RequestError, если соединение с сервером невозможно
+            или код ответа сервера != 200.
+        '''
+
         try:
             response = requests.post(
                 url=self.__url,
@@ -51,6 +78,14 @@ class ApiGetAndParse:
             return response
 
     def parse_rates_data(self) -> list[dict]:
+        '''
+        Обрабатывает ответ сервера.
+
+        Возвращает:
+            Данные о курсах валют за заданную дату в формате
+            списка словарей.
+        '''
+
         text = xmltodict.parse(self.get_rates_data().text)
         rates_data = text.get(
             'soap:Envelope'
@@ -69,6 +104,17 @@ class ApiGetAndParse:
         return rates_data
 
     def get_required_currencies(self, codes: list[str]) -> list[dict] | bool:
+        '''
+        Формирует список с данными запрошенных пользователем валют.
+
+        Аргументы:
+            codes: список, содержащий запрошенные коды в виде строк.
+
+        Возвращает:
+            Список словарей с данными о запрошенных валютах или False,
+            если пользователь задал несуществующие коды валют.
+        '''
+
         currency_list = [
             i for i in self.parse_rates_data() if i.get('Vcode') in codes
         ]
