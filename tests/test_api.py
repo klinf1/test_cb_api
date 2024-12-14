66f1fd42764516fbca36583140ee19c5135e36eb
Михаил Манузин <klinf157@gmail.com>
2024-12-14 21:45:22 +0300

    added a fixture for getting example response for api testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,6 +8,17 @@ from exceptions import RequestError
 date = '14.12.2024'
 
 
+@pytest.fixture(scope='function')
+def get_test_response():
+    try:
+        with open('tests/example_response.txt', 'r', encoding='utf-8') as f:
+            example_text = f.read()
+    except Exception:
+        print('Ошибка! Файл с тестовым ответом сервера не найден.')
+    else:
+        return example_text
+
+
 def test_get_rates_data_good_status_code():
     with patch('requests.post') as mock_post:
         mock_post.return_value.status_code = 200
@@ -27,12 +38,10 @@ def test_get_rates_data_bad_status_code():
         assert e_info.type is RequestError, err_msg
 
 
-def test_parse_rates_data():
-    with open('tests/example_response.txt', 'r', encoding='utf-8') as f:
-        example_text = f.read()
+def test_parse_rates_data(get_test_response):
     with patch('requests.post') as mock_post:
         mock_post.return_value.status_code = 200
-        mock_post.return_value.text = example_text
+        mock_post.return_value.text = get_test_response
         a = api.ApiGetAndParse(date).parse_rates_data()
         err_msg = 'Проверьте, что при корректном ответе сервера parse_'
         err_msg += 'rates_data возвращает список словарей с курсами валют'
@@ -41,12 +50,10 @@ def test_parse_rates_data():
         assert a[0].get('Vname') == 'Австралийский доллар', err_msg
 
 
-def test_get_required_currencies():
-    with open('tests/example_response.txt', 'r', encoding='utf-8') as f:
-        example_text = f.read()
+def test_get_required_currencies(get_test_response):
     with patch('requests.post') as mock_post:
         mock_post.return_value.status_code = 200
-        mock_post.return_value.text = example_text
+        mock_post.return_value.text = get_test_response
         a = api.ApiGetAndParse(date).get_required_currencies(['36', '000'])
         err_msg = 'Проверьте, что get_required_currencies '
         err_msg += 'возвращает данные только для корректных кодов'
